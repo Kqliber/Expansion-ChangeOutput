6fc97024f31549ffeec43d7f14d9abe2f798b639
Kqliber <kqliber@gmail.com>
2022-06-26 21:06:33 +0100

    ok fine ill fix it

--- src/main/kotlin/me/kaliber/expansions/changeoutput/ChangeOutputExpansion.kt
+++ src/main/kotlin/me/kaliber/expansions/changeoutput/ChangeOutputExpansion.kt
@@ -5,10 +5,13 @@ import me.clip.placeholderapi.PlaceholderAPI
 import org.bukkit.OfflinePlayer
 import org.bukkit.ChatColor
 
+const val INPUT_MATCHER = "_input:"
+const val MATCHER_MATCHER = "_matcher:"
+const val IFMATCH_MATCHER = "_ifmatch:"
+const val ELSE_MATCHER = "_else:"
+
 class ChangeOutputExpansion : PlaceholderExpansion()
 {
-
-    private val separator = Regex("(?<!\\\\)_")
 
     override fun getIdentifier(): String
     {
@@ -27,54 +30,72 @@ class ChangeOutputExpansion : PlaceholderExpansion()
 
     override fun onRequest(player: OfflinePlayer?, params: String): String?
     {
-        val (opts, input, checkAgainst, outputIfTrue, outputIfFalse) = PlaceholderAPI.setBracketPlaceholders(player, params)
-            .split(separator)
-            .map { it.replace("\\_", "_") }
-            .takeIf { it.size >= 5 } ?: return null
+        if (!params.contains(INPUT_MATCHER) ||
+            !params.contains(MATCHER_MATCHER) ||
+            !params.contains(IFMATCH_MATCHER) ||
+            !params.contains(ELSE_MATCHER))
+        {
+            return null
+        }
 
-        val options = opts.lowercase().split(",")
+        val parsed = PlaceholderAPI.setBracketPlaceholders(player, params)
+
+        val options = parsed.substringBefore(INPUT_MATCHER).lowercase().split(',')
         if (options.size > 3) return null
 
+        val input = parsed.substringAfter(INPUT_MATCHER).substringBefore(MATCHER_MATCHER)
+        val matcher = parsed.substringAfter(MATCHER_MATCHER).substringBefore(IFMATCH_MATCHER)
+        val outputIfTrue = parsed.substringAfter(IFMATCH_MATCHER).substringBefore(ELSE_MATCHER)
+        val outputIfFalse = parsed.substringAfter(ELSE_MATCHER)
+        println(input)
+        println(matcher)
+        println(outputIfTrue)
+        println(outputIfFalse)
+        println("contains: ${matcher in input}")
+
         val inputNum = input.toDoubleOrNull() ?: 0.0
-        val checkNum = checkAgainst.toDoubleOrNull() ?: 0.0
+        val checkNum = matcher.toDoubleOrNull() ?: 0.0
 
         if (options.size == 1)
         {
             return when (options[0].lowercase())
             {
-                "ignorecase" -> if (input.equals(checkAgainst, true)) outputIfTrue else outputIfFalse
-                "ignorecolor" -> if (ChatColor.stripColor(input) == checkAgainst) outputIfTrue else outputIfFalse
-                "contains" -> if (checkAgainst in input) outputIfTrue else outputIfFalse
+                "ignorecase" -> if (input.equals(matcher, true)) outputIfTrue else outputIfFalse
+                "ignorecolor" -> if (ChatColor.stripColor(input) == matcher) outputIfTrue else outputIfFalse
+                "contains" -> {
+                    println("size 1")
+                    if (matcher in input) outputIfTrue else outputIfFalse
+                }
 
                 ">" -> if (inputNum > checkNum) outputIfTrue else outputIfFalse
                 ">=" -> if (inputNum >= checkNum) outputIfTrue else outputIfFalse
                 "<" -> if (inputNum < checkNum) outputIfTrue else outputIfFalse
                 "<=" -> if (inputNum <= checkNum) outputIfTrue else outputIfFalse
 
-                else -> if (input == checkAgainst) outputIfTrue else outputIfFalse
+                else -> if (input == matcher) outputIfTrue else outputIfFalse
             }
         }
 
         var newInput = input
-        var newCheckAgainst = checkAgainst
+        var newMatcher = matcher
 
         if (options.contains("ignorecase"))
         {
             newInput = newInput.lowercase()
-            newCheckAgainst = newCheckAgainst.lowercase()
+            newMatcher = newMatcher.lowercase()
         }
 
         if (options.contains("ignorecolor"))
         {
             newInput = ChatColor.stripColor(newInput) ?: ""
-            newCheckAgainst = ChatColor.stripColor(newCheckAgainst) ?: ""
+            newMatcher = ChatColor.stripColor(newMatcher) ?: ""
         }
 
         if (options.contains("contains"))
         {
-            return if (newCheckAgainst in newInput) outputIfTrue else outputIfFalse
+            return if (newMatcher in newInput) outputIfTrue else outputIfFalse
         }
 
-        return if (newInput == newCheckAgainst) outputIfTrue else outputIfFalse
+        return if (newInput == newMatcher) outputIfTrue else outputIfFalse
     }
 }
